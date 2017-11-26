94a6d32742d5153f3918070eee6a3763851b7dfb
DESKTOP-E928IJT\Tran Duc Quang <ducquangcit@gmail.com>
2017-11-26 10:56:44 +0700

    The first commit

---
+++ ShopExample.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopExample.Web.Startup))]
+namespace ShopExample.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
