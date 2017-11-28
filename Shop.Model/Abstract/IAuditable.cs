4c402d14c5109d190773b337ba8c3efa1fce8477
DESKTOP-E928IJT\Tran Duc Quang <ducquangcit@gmail.com>
2017-11-28 07:19:16 +0700

    Domain Entites

---
+++ Shop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Shop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreateDate { set; get; }
+        string CreateBy { set; get; }
+        DateTime? UpdateDate { set; get; }
+        string UpdateBy { set; get; }
+        string MetaKeyWord { set; get; }
+        string MetaDescription { set; get; }
+        bool Status { set; get; }
+    }
+}
---
+++ Shop.Model/Model/ProductCategories.cs
@@ -0,0 +1,30 @@
+﻿using Shop.Model.Abstract;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shop.Model.Model
+{
+    public class ProductCategories : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        [Required]
+        public string Name { set; get; }
+
+        [Required]
+        public string Alias { set; get; }
+
+        public string Description { set; get; }
+
+        public int? ParentID { set; get; }
+
+        public int? DisplayOrder { set; get; }
+
+        public bool? HomeFlag { set; get; }
+
+        public virtual IEnumerable<Product> Products { set; get; }
+    }
+}
